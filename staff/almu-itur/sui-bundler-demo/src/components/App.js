a6e13675d0b448d2a94151e3f6b23ac2e4db0811
Almu Iturgoyen <almudena.iturgoyen@gmail.com>
2018-12-10 17:35:14 +0100

    firs steps, first component and first imported button

---
+++ staff/almu-itur/sui-bundler-demo/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '@schibstedspain/sui-atom-button'
+
+const App = () => (
+  <div>
+    <h1>Hola mundo</h1>
+    <Button onClick={() => window.alert('Primary with onClick')}>
+      Primary with onClick
+    </Button>
+  </div>
+)
+
+export default App
