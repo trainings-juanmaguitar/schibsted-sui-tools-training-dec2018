f5d848fd8796cb8295e6eb0c6619483bec5ffebd
Lluis Bagur <lluisbagur09@gmail.com>
2018-12-10 17:32:19 +0100

    sui

---
+++ staff/lluis-bagur/sui-bundler-demo/src/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+
+import App from './components/App'
+
+debugger //eslint-disable-line
+console.log('hey')
+
+ReactDOM.render(<App />, document.getElementById('⚛️'))
---
+++ staff/lluis-bagur/sui-bundler-demo/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => (
+  <div>
+    <h1> Hola Mundo </h1>
+    <Button onClick={() => window.alert('Primary with onClick')}>
+      Primary with onClick
+    </Button>
+    <Button type="accent" title="Title: Lorem Ipsum">
+      Accent with title
+    </Button>
+    <Button type="secondary" className="customClass">
+      Secondary with className
+    </Button>
+  </div>
+)
+
+export default App
