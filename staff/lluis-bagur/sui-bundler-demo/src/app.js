acd684ab18e818d2daa2bac2ad6380d8248b30b2
Lluis Bagur <lluisbagur09@gmail.com>
2018-12-11 17:26:24 +0100

    feat(components): add contact form

--- staff/lluis-bagur/sui-bundler-demo/src/app.js
+++ staff/lluis-bagur/sui-bundler-demo/src/app.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.scss'
 
 import App from './components/App'
+import Form from './components/form/form.js'
 
 ReactDOM.render(<App />, document.getElementById('⚛️'))
+ReactDOM.render(<Form />, document.getElementById('⚛️'))
--- staff/lluis-bagur/sui-bundler-demo/src/components/App.js
+++ staff/lluis-bagur/sui-bundler-demo/src/components/App.js
@@ -1,18 +1,8 @@
 import React from 'react'
-import Button from '@schibstedspain/sui-atom-button'
 
 const App = () => (
-  <div>
+  <div className="form">
     <h1> Hola Mundo </h1>
-    <Button onClick={() => window.alert('Primary with onClick')}>
-      Primary with onClick
-    </Button>
-    <Button type="accent" title="Title: Lorem Ipsum">
-      Accent with title
-    </Button>
-    <Button type="secondary" className="customClass">
-      Secondary with className
-    </Button>
   </div>
 )
 
