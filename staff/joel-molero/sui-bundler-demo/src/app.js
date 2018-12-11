93328c279f6c0ec90ea96061ee2cf39e0b96581a
Joel Molero <joel.molero@MacBook-Pro-de-Adriana.local>
2018-12-11 17:34:28 +0100

    feat(Root): add form and foo function

--- staff/joel-molero/sui-bundler-demo/src/app.js
+++ staff/joel-molero/sui-bundler-demo/src/app.js
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './components/App'
+import Foo from './foo'
 
 import './index.scss'
+
+Foo()
 
 ReactDOM.render(<App />, document.getElementById('⚛️'))
