aefb2f40ec0da185ac0b95d54609a7d0158909cc
henry <henry.r.novoa@gmail.com>
2018-12-11 17:26:31 +0100

    feat(Root): add babel compatability and import as chunks

--- staff/henry-novoa/sui-bundler-demo/src/app.js
+++ staff/henry-novoa/sui-bundler-demo/src/app.js
@@ -2,5 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import './index.scss'
+import(/* webpackChunkName: "my-chunk-name" */ './foo').then(
+  ({default: foo}) => {
+    console.log('loaded async chunk')
+    foo()
+  }
+)
 
 ReactDOM.render(<App />, document.getElementById('⚛️'))
