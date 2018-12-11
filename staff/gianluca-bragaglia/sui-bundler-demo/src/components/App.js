9372fec20f6feeaf23c13584a2b84440a9f5c178
Gianluca <gianluca.bragaglia@gmail.com>
2018-12-11 16:49:23 +0100

    feat(Root): add a form components

--- staff/gianluca-bragaglia/sui-bundler-demo/src/components/App.js
+++ staff/gianluca-bragaglia/sui-bundler-demo/src/components/App.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import Button from '@schibstedspain/sui-atom-button'
+import Form from './Form'
 
 const App = () => (
   <div>
-    <h1>Hola Mundo</h1>
-    <Button onClick={() => window.alert('Primary with onClick')}>
-      Primary with onClick
-    </Button>
+    <Form />
   </div>
 )
 
