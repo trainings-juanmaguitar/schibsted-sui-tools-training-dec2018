ec16c83a4daab24b93d74273661e70caba1642ec
Joel Molero <joel.molero@MacBook-Pro-de-Adriana.local>
2018-12-11 15:16:38 +0100

    feat(Root): readme added & dependencies updated following comver

---
+++ staff/joel-molero/sui-bundler-demo/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from '@schibstedspain/sui-atom-button'
+
+const BASE_CLASS = 'MyApp'
+
+const App = () => (
+  <div className={BASE_CLASS}>
+    <h1>Hola Mundo</h1>
+    <Button onClick={() => window.alert('Primary with onClick')}>
+      Primary with onClick
+    </Button>
+  </div>
+)
+
+export default App
