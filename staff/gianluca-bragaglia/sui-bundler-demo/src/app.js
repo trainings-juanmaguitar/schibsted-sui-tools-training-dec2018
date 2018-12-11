ca6f2d84c938d073bca3576ff65bc06b826068d2
Gianluca <gianluca.bragaglia@gmail.com>
2018-12-11 17:26:18 +0100

    feat(Root): add foo.js

--- staff/gianluca-bragaglia/sui-bundler-demo/src/app.js
+++ staff/gianluca-bragaglia/sui-bundler-demo/src/app.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+import foo from './foo'
 
 import './index.scss'
+
+foo()
 
 ReactDOM.render(<App />, document.getElementById('app'))
