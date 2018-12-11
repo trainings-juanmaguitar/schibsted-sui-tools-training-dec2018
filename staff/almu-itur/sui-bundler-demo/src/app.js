2c4749bcd3eb6bba235b80e1ee213c1a8e2a5bcd
Almu Iturgoyen <almudena.iturgoyen@gmail.com>
2018-12-11 17:25:38 +0100

    feat(components): add foo function

--- staff/almu-itur/sui-bundler-demo/src/app.js
+++ staff/almu-itur/sui-bundler-demo/src/app.js
@@ -5,6 +5,8 @@ import App from './components/App'
 
 import './index.scss'
 
+import foo from './foo'
+
 console.log('Hi!') //eslint-disable-line
 
 ReactDOM.render(<App />, document.getElementById('app'))
