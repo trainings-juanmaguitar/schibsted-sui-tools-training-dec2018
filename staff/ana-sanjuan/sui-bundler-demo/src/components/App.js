c9125968fcf4fc24f8b7926db58606f921790933
adancing <anatsanjuan@gmail.com>
2018-12-11 16:48:10 +0100

    feat(components): add form component

--- staff/ana-sanjuan/sui-bundler-demo/src/components/App.js
+++ staff/ana-sanjuan/sui-bundler-demo/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Button from '@schibstedspain/sui-atom-button'
+import Form from './Form'
 
 const BASE_CLASS = 'MyApp'
 
@@ -7,12 +7,7 @@ const App = () => {
   return (
     <div className={BASE_CLASS}>
       <h1>Hola Mundo</h1>
-      <Button
-        type="secondary"
-        onClick={() => window.alert('Primary with onClick')}
-      >
-        Primary with onClick
-      </Button>
+      <Form />
     </div>
   )
 }
