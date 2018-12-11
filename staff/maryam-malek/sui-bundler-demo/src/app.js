3d13c47af22a1b8202b9639c4a955c44e089110d
maryamdot <maryamdot@maryamdots-MacBook-Pro-2.local>
2018-12-11 15:17:29 +0100

    feat(Root): first commit

---
+++ staff/maryam-malek/sui-bundler-demo/src/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+import './index.scss'
+// import './index.1.sass'
+
+console.log('hey') //eslint-disable-line
+
+ReactDOM.render(<App />, document.getElementById('⚛️'))
---
+++ staff/maryam-malek/sui-bundler-demo/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from '@schibstedspain/sui-atom-button'
+
+const BASE_CLASS = 'MyApp'
+
+const App = () => (
+  <div className={BASE_CLASS}>
+    <h1>Hola Mundo!</h1>
+    <Button
+      type="terciary"
+      onClick={() => window.alert('Primary with onClick')}
+    >
+      Primary with onClick
+    </Button>
+  </div>
+)
+
+export default App
