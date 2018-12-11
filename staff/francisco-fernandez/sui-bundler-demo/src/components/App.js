b35a489c94893f96c91a54adfdee9bce356a1a2f
Paco <franfercant@gmail.com>
2018-12-11 16:45:29 +0100

    feat(components): new component form created

--- staff/francisco-fernandez/sui-bundler-demo/src/components/App.js
+++ staff/francisco-fernandez/sui-bundler-demo/src/components/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import Button from '@schibstedspain/sui-atom-button'
+// import Button from '@schibstedspain/sui-atom-button'
+import Form from './Form'
 const BASE_CLASS = 'MyApp'
+const FORM_CLASS = 'MyForm'
 
 const App = () => (
   <div className={BASE_CLASS}>
     <h1>Hola Mundo</h1>
-    <Button onClick={() => window.alert('Primary with onClick')}>
+    {/* <Button onClick={() => window.alert('Primary with onClick')}>
       Primary with onClick
-    </Button>
+    </Button> */}
+    <Form className={FORM_CLASS} />
   </div>
 )
 
