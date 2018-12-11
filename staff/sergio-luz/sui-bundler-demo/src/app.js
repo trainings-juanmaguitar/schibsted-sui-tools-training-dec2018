311c0545d07f6198135556961c084974d9681c30
Sergio <sergio.luz.fernandez@gmail.com>
2018-12-11 15:17:34 +0100

    feat(Root): readme added & dependencies updated following comver

---
+++ staff/sergio-luz/sui-bundler-demo/src/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './components/App'
+
+import './index.scss'
+
+// console.log('hey!')
+
+ReactDOM.render(<App />, document.getElementById('⚛️'))
