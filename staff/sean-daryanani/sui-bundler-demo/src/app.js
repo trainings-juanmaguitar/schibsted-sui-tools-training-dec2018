c2875dcfa7820d7baea9355cdbea28f317fdb570
Sean Daryanani <seand52@gmail.com>
2018-12-11 17:29:36 +0100

    feat(components): added form with sui components

--- staff/sean-daryanani/sui-bundler-demo/src/app.js
+++ staff/sean-daryanani/sui-bundler-demo/src/app.js
@@ -2,5 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import './index.scss'
+import Foo from './foo'
 
+Foo()
 ReactDOM.render(<App />, document.getElementById('⚛️'))
