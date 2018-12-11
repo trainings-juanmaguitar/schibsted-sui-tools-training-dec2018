90a80b3bac7624d783d8df111ffe19fffdeda1ac
Daniel Villegas <dani@MacBook-Pro-de-Daniel.local>
2018-12-11 16:49:12 +0100

    feat(components): add formulary component
    added fomrulary component with inputs, textarea and submit button
    
    BREAKING CHANGE:
    s

--- staff/daniel-villegas/sui-bundler-demo/src/app.js
+++ staff/daniel-villegas/sui-bundler-demo/src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import './index.scss'
 
-console.log('yep')//eslint-disable-line
-debugger//eslint-disable-line
+// console.log('yep') //eslint-disable-line
+// debugger //eslint-disable-line
 
 ReactDOM.render(<App />, document.getElementById('⚛️'))
--- staff/daniel-villegas/sui-bundler-demo/src/components/App.js
+++ staff/daniel-villegas/sui-bundler-demo/src/components/App.js
@@ -1,18 +1,12 @@
 import React from 'react'
-import Button from '@schibstedspain/sui-atom-button'
+import Form from './Form/index'
 
 const BASE_CLASS = 'MyApp'
 
 const App = () => (
   <div className={BASE_CLASS}>
     <h1>Hola Mundo</h1>
-    <Button
-      type="primary"
-      class="sui-AtomButton sui-AtomButton--link"
-      onClick={() => window.alert('yep!')}
-    >
-      Normal
-    </Button>
+    <Form />
   </div>
 )
 
