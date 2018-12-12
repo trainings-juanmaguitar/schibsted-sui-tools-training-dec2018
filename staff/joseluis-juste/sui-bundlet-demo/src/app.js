d878a3d3a28d033118ecb3eb875fea14a93cc8d9
scrafy <scrafy26@gmail.com>
2018-12-12 12:11:46 +0100

    feat(Root): Created form using sui components

---
+++ staff/joseluis-juste/sui-bundlet-demo/src/app.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import Form from './components/Form/Form'
+
+ReactDOM.render(<Form />, document.getElementById('⚛️'))
