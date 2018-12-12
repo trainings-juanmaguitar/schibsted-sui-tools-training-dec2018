6d0648917a12d874bca04efec37e1b4e4fc3fc3a
Gianluca <gianluca.bragaglia@gmail.com>
2018-12-12 17:19:45 +0100

    feat(components): add routes, and other components

--- staff/gianluca-bragaglia/sui-bundler-demo/src/app.js
+++ staff/gianluca-bragaglia/sui-bundler-demo/src/app.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
-import foo from './foo'
+// import foo from './foo'
+import {register} from '@s-ui/bundler/registerServiceWorker'
+import Router from 'react-router/lib/Router'
+import {browserHistory} from 'react-router'
+
+import routes from './routes'
 
 import './index.scss'
 
-foo()
+register({
+  first: () => window.alert('Content is cached for offline use.'),
+  renovate: () => window.alert('New content is available; please refresh.')
+})()
 
-ReactDOM.render(<App />, document.getElementById('app'))
+// foo()
+
+// eslint-next-disable-line
+import(/* webpackChunkName: "my-chunk-name" */ './foo').then(
+  ({default: foo}) => {
+    console.log('loaded async chunk') // eslint-disable-line
+    foo()
+  }
+)
+
+ReactDOM.render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app')
+)
