392579ff88b8faa4ffa30e52d374d1f5ecc2a885
Paco <franfercant@gmail.com>
2018-12-12 09:13:15 +0100

    feat(Root): new feature foo.js

--- staff/francisco-fernandez/sui-bundler-demo/src/app.js
+++ staff/francisco-fernandez/sui-bundler-demo/src/app.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import App from './components/App'
-
 import './index.scss'
+// import foo from './foo'
+
+// foo()
+
+// eslint-next-disable-line
+import(/* webpackChunkName: "my-chunk-name" */ './foo').then(
+  ({default: foo}) => {
+    console.log('loaded async chunk')
+    foo()
+  }
+)
 
 ReactDOM.render(<App />, document.getElementById('⚛️'))
