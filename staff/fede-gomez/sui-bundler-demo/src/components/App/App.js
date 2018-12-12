2cae0763edd182524fa87762ddc992a5a3fe6724
Fede <fedegomezcucchiararo@gmail.com>
2018-12-12 14:07:11 +0100

    feat(examples): add example webpack chunk

--- staff/fede-gomez/sui-bundler-demo/src/components/App/App.js
+++ staff/fede-gomez/sui-bundler-demo/src/components/App/App.js
@@ -1,10 +1,22 @@
+/* eslint-disable no-console */
+
 import React from 'react'
 import Form from '../Form/Form'
-import foo from '../../foo'
 
 const BASE_CLASS = 'MyApp'
 
-foo()
+/*  the next piece of code allows us to use Webpack to load different chunks and load them via JavaScript.
+    this might increase performance, since we load only the chunks that we need e.g. Home, App, etc.
+
+    We are executing a funciton in an asynchronous way
+*/
+// eslint-next-disable-line
+import(/* webpackChunkName: "my-chunk-name" */ '../../foo').then(
+  ({default: foo}) => {
+    console.log('loaded async chunk')
+    foo()
+  }
+)
 
 const App = () => {
   return (
