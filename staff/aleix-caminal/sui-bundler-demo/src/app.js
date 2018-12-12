536600fb37f36cf85a4e1e503fa18dacfd575ea6
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-12-12 15:10:07 +0100

    feat(Root): first build

--- staff/aleix-caminal/sui-bundler-demo/src/app.js
+++ staff/aleix-caminal/sui-bundler-demo/src/app.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+
 import './index.scss'
+
+import {register} from '@s-ui/bundler/registerServiceWorker'
+
+register({
+  first: () => window.alert('Content is cached for offline use.'),
+  renovate: () => window.alert('New content is available; please refresh.')
+})()
 
 ReactDOM.render(<App />, document.getElementById('root'))
--- staff/aleix-caminal/sui-bundler-demo/src/components/App/App.js
+++ staff/aleix-caminal/sui-bundler-demo/src/components/App/App.js
@@ -4,7 +4,7 @@ import Form from '../Form'
 function App() {
   return (
     <div className="app">
-      <h1 className="app__title">Register</h1>
+      <h1 className="app__title">Demo Form</h1>
       <Form />
     </div>
   )
