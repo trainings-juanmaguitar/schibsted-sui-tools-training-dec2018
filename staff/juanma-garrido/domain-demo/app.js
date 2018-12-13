1d543e75e95f348d6f6304b6cc5a65c414c6628f
JuanMa Garrido <juanma.garrido@gmail.com>
2018-12-13 15:36:34 +0100

    feat(Root): demo domain juanma

---
+++ staff/juanma-garrido/domain-demo/app.js
@@ -0,0 +1,5 @@
+import Domain from './src'
+
+const domain = new Domain()
+
+domain.get('list_students_use_case').execute().then(console.log)
---
+++ staff/juanma-garrido/domain-demo/src/students/UseCases/ListStudentsUseCase.js
@@ -0,0 +1,11 @@
+import { UseCase } from '@s-ui/domain'
+
+class ListStudentsUseCase extends UseCase {
+  constructor(repository) {
+    super()   
+    this._repository =repository
+  }
+  execute() {
+    
+  }
+}
