19f2f5b1f5b55b136a1ad3a523acb73533de7ab0
Daniel Villegas <dani@MacBook-Pro-de-Daniel.local>
2018-12-13 14:35:57 +0100

    perf(components): add pages

---
+++ staff/daniel-villegas/sui-bundler-demo/public/List.fd8e6cd8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{izyu:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var u=function(e){return e&&e.__esModule?e:{default:e}}(t("mXGw"));n.default=function(){return u.default.createElement("div",null,u.default.createElement("h1",null,"LIST TEXT"))}}}]);
--- staff/daniel-villegas/sui-bundler-demo/src/app.js
+++ staff/daniel-villegas/sui-bundler-demo/src/app.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
+// import App from './components/App'
+import Router from 'react-router/lib/Router'
+import {browserHistory} from 'react-router'
+import routes from './routes'
 import './index.scss'
-import(/* webpackChunkName: "my-chunk-name" */ './foo').then(
-  ({default: foo}) => {
-    console.log('loaded async chunk')
-    foo()
-  }
-)
+// import(/* webpackChunkName: "my-chunk-name" */ './foo').then(
+//   ({default: foo}) => {
+//     console.log('loaded async chunk') //eslint-disable-line
+//     foo()
+//   }
+// )
+import {register} from '@s-ui/bundler/registerServiceWorker'
+
+register({
+  first: () => window.alert('Content is cached for offline use.'),
+  renovate: () => window.alert('New content is available; please refresh.')
+})()
 
 // console.log('yep') //eslint-disable-line
 // debugger //eslint-disable-line
 
-ReactDOM.render(<App />, document.getElementById('⚛️'))
+ReactDOM.render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('⚛️')
+)
