ac209f37245009e73889c8a5b2e5ce149e0c3ef7
Joel Molero <joel.molero@MacBook-Pro-de-Adriana.local>
2018-12-13 14:33:08 +0100

    feat(Root): added react router and offline availability

--- staff/joel-molero/sui-bundler-demo/src/app.js
+++ staff/joel-molero/sui-bundler-demo/src/app.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Router from 'react-router/lib/Router'
+import {browserHistory} from 'react-router'
 
-import App from './components/App'
-import Foo from './foo'
+import routes from './routes'
 
 import './index.scss'
 
-Foo()
+import {register} from '@s-ui/bundler/registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('⚛️'))
+register({
+  first: () => window.alert('Content is cached for offline use.'),
+  renovate: () => window.alert('New content is available; please refresh.')
+})()
+
+ReactDOM.render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('root')
+)
