19e5fd7a77b28c8deee7cf1a6d56d3e6238c21df
scrafy <scrafy26@gmail.com>
2018-12-13 14:38:59 +0100

    feat(Root): added react router & offline way of work

--- staff/joseluis-juste/sui-bundlet-demo/src/app.js
+++ staff/joseluis-juste/sui-bundlet-demo/src/app.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
-import Form from './components/Form/Form'
+import {register} from '@s-ui/bundler/registerServiceWorker'
+import Router from 'react-router/lib/Router'
+import {browserHistory} from 'react-router'
 
-ReactDOM.render(<Form />, document.getElementById('⚛️'))
+import routes from './routes'
+
+register({
+  first: () => window.alert('Content is cached for offline use.'),
+  renovate: () => window.alert('New content is available; please refresh.')
+})()
+
+ReactDOM.render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('root')
+)
