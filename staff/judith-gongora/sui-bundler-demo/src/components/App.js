5dd8a6c3d9902ce086775f4c6a192b0013cec42e
Judith Gongora Cruz <judith.gongora.cruz@gmail.com>
2018-12-13 14:34:35 +0100

    feat(Root): add react router

--- staff/judith-gongora/sui-bundler-demo/src/components/App.js
+++ staff/judith-gongora/sui-bundler-demo/src/components/App.js
@@ -1,15 +1,31 @@
 import React from 'react'
-import Button from '@schibstedspain/sui-atom-button'
+import Helmet from 'react-helmet'
+import PropTypes from 'prop-types'
 
-function App() {
+import Link from 'react-router/lib/Link'
+
+console.log('App..') // eslint-disable-line
+
+const App = (props, data) => {
+  console.log(props, data) // eslint-disable-line
+  const {children} = props
   return (
-    <div>
-      <h1>Hola Mundo</h1>
-      <Button onClick={() => window.alert('Primary with onClick')}>
-        Primary with onClick
-      </Button>
+    <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Helmet application" />
+        <title>SPA MOCK</title>
+      </Helmet>
+      <nav>
+        <h1>App</h1>
+        <Link to="/">Home</Link>
+        <Link to="/list">List</Link>
+        <Link to="/detail">Detail</Link>
+      </nav>
+      {children}
     </div>
   )
 }
+App.propTypes = {children: PropTypes.element}
 
 export default App
