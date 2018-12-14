b5ff95041cb23802cb5c90965c81ea7185cc53f2
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-12-14 15:36:10 +0100

    feat(Root): added context and students list to home page

--- staff/aleix-caminal/domain-demo/src/students/UseCases/factory.js
+++ staff/aleix-caminal/domain-demo/src/students/UseCases/factory.js
@@ -5,7 +5,7 @@ class StudentsUseCasesFactory {
   static listStudentsUseCase = ({config}) =>
     new ListStudentsUseCase({
       config,
-      repository: StudentsRepositoriesFactory.rawStudentsRepository
+      repository: StudentsRepositoriesFactory.rawStudentsRepository({config})
     })
 }
 
--- staff/aleix-caminal/sui-bundler-demo/src/pages/Home/index.js
+++ staff/aleix-caminal/sui-bundler-demo/src/pages/Home/index.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
-function Home(props) {
-  return (
-    <React.Fragment>
-      <h1>Home</h1>
-    </React.Fragment>
-  )
+class Home extends Component {
+  state = {
+    students: []
+  }
+
+  static contextTypes = {
+    domain: PropTypes.object
+  }
+
+  async componentDidMount() {
+    const students = await this.context.domain
+      .get('list_students_use_case')
+      .execute()
+
+    this.setState({students})
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Home</h1>
+        {this.state.students &&
+          this.state.students.length > 0 &&
+          this.state.students.map((student, index) => (
+            <p key={index}>{student.name}</p>
+          ))}
+      </React.Fragment>
+    )
+  }
 }
 
 export default Home
