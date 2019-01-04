76e810a788ec4253f508ef99cb474185aad5e877
JuanMa Garrido <juanma.garrido@gmail.com>
2019-01-04 20:07:33 +0100

    feat(demos/schibsted-frontend-mv-project): removed credentials

--- stuff/demos/schibsted-frontend-mv-project/packages/frontend-mv--lib-movies/src/config/base.js
+++ stuff/demos/schibsted-frontend-mv-project/packages/frontend-mv--lib-movies/src/config/base.js
@@ -1,7 +1,4 @@
-import credentials from './credentials'
-
 export default {
-  THEMOVIEDB_API_KEY: credentials.API_KEY,
   THEMOVIEDB_API_BASE_URL: 'api.themoviedb.org/3',
   FIREBASE_API_URL: 'http://localhost:5000/moviesfinder-ecb7c/us-central1/api',
   IMAGE_BASE_URL: 'https://image.tmdb.org/t/p/w300_and_h450_bestv2',
